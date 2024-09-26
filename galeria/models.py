e92f80b5de7bea6c6fc63469c3373bc99cb8cd96
Bruno de Moraes <brunoestudolog@gmail.com>
2024-09-26 20:18:50 -0300

    Primeira migração de banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descrição = models.TextField(null=False, blank=False)
+    fotos = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return f'Fotograia [nome={self.nome}]'
--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+    1: {'nome': 'Nebulosa de Carina',
+        'legenda': 'webbtelescope.org / Nasa / James Webb'
+        },
+    2: {'nome': 'Galaxia NGC 1079',
+        'legenda': 'nasa.org / Nasa / Hubble'
+        }
+}
+
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 
 def imagem(request):
